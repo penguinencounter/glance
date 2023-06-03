cc0fe1a1d5a297ff09e51685e87624abd8d3fcb9
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2023-06-03 10:58:28 -0700

    Join the Webpack revolution!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-/// <reference path="./node_modules/jquery/dist/jquery.js" />
-/// <reference path="./node_modules/oojs/dist/oojs.js" />
-/// <reference path="./node_modules/oojs-ui/dist/oojs-ui.js" />
-/// <reference path="./node_modules/oojs-ui/dist/oojs-ui-wikimediaui.js" />
+/// <reference path="../node_modules/jquery/dist/jquery.js" />
+/// <reference path="../node_modules/oojs/dist/oojs.js" />
+/// <reference path="../node_modules/oojs-ui/dist/oojs-ui.js" />
+/// <reference path="../node_modules/oojs-ui/dist/oojs-ui-wikimediaui.js" />
 
 function GlanceOptions(config) {
     GlanceOptions.super.apply(this, config)
