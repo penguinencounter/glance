eb20b28b774aaa4f80c234eadd9faba86a086d5a
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2023-06-03 15:14:04 -0700

    Switch webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     mode: "development",
-    devtool: false,
+    // devtool: false,
     module: {
         rules: [
             {
