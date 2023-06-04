788b4968906f11030f3e574d6cd42ee90525c83f
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2023-06-04 11:11:05 -0700

    (debugging metadata detection)

--- src/meta.ts
+++ src/meta.ts
@@ -3,14 +3,16 @@ import shared from "./shared";
 export default {
     ready: function () {
         // set up a MutationObserver to detect when the page changes
-        const observer = new MutationObserver(function (mutations) {
+        const f = () => {
             const meta = shared.getMetadata()
             if (meta.has("glanceAllowMeta")) {
                 ;(document.querySelectorAll(".glance-hide-installed") as NodeListOf<HTMLElement>).forEach( e => {
                     e.style.display = "none";
                 })
             }
-        })
+        }
+        f();
+        const observer = new MutationObserver(f)
         observer.observe(document.body, {
             childList: true,
             subtree: true
